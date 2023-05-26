4cc4f2853827cf015ef157769e132f601d0781c0
lucas-caminha <lucas_santoscaminha@hotmail.com>
2023-05-26 20:17:35 -0300

    commit inicial do projeto

---
+++ src/main/java/br/com/bahia/backend/notificacao/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.bahia.backend.notificacao;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NotificacaoApplication.class);
+	}
+
+}
